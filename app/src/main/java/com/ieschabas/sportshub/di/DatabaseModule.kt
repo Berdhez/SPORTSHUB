4d39e4ac11eecad6f15a75c935e809641541ba5f
berni <berdhez90@gmail.com>
2026-01-29 03:30:00 +0100

    DatabaseModule creado

--- app/src/main/java/com/ieschabas/sportshub/di/DatabaseModule.kt
+++ app/src/main/java/com/ieschabas/sportshub/di/DatabaseModule.kt
@@ -1,2 +1,25 @@
 package com.ieschabas.sportshub.di
 
+import android.content.Context
+import androidx.room.Room
+import com.ieschabas.sportshub.data.local.SportsHubDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDb(@ApplicationContext context: Context): SportsHubDatabase =
+        Room.databaseBuilder(context, SportsHubDatabase::class.java, "sportshub.db")
+            .build()
+
+    @Provides
+    fun provideClassificationDao(db: SportsHubDatabase) = db.classificationDao()
+}
