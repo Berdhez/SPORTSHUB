948e3f10c1de40469451cfdea2df1a2669ca475c
berni <berdhez90@gmail.com>
2026-01-29 03:32:30 +0100

    RepositoryModule creado

--- app/src/main/java/com/ieschabas/sportshub/di/RepositoryModule.kt
+++ app/src/main/java/com/ieschabas/sportshub/di/RepositoryModule.kt
@@ -1,2 +1,18 @@
 package com.ieschabas.sportshub.di
 
+import com.ieschabas.sportshub.data.repository.ClassificationRepositoryImpl
+import com.ieschabas.sportshub.domain.repository.ClassificationRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    abstract fun bindClassificationRepository(
+        impl: ClassificationRepositoryImpl
+    ): ClassificationRepository
+}
