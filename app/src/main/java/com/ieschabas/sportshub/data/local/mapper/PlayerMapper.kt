e34e0059bfc4f15df3293f0754071fd77c64a153
Josep <josnavllo3@alu.edu.gva.es>
2026-02-04 16:43:16 +0100

    he arreglado PlayerMapper.kt

--- app/src/main/java/com/ieschabas/sportshub/data/local/mapper/PlayerMapper.kt
+++ app/src/main/java/com/ieschabas/sportshub/data/local/mapper/PlayerMapper.kt
@@ -3,6 +3,16 @@ package com.ieschabas.sportshub.data.local.mapper
 import com.ieschabas.sportshub.data.local.entities.PlayerEntity
 import com.ieschabas.sportshub.domain.model.Player
 
+fun PlayerEntity.toDomain() = Player(
+    id = id,
+    teamId = teamId,
+    name = name,
+    position = position,
+    number = number,
+    nationality = nationality
+)
+
+
 fun Player.toEntity() = PlayerEntity(
     id = id,
     teamId = teamId,
